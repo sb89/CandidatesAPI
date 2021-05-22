4732ff32ae528e6eff76064dc78d1f7a020cacf1
Steven Blake <s.blake.191289@gmail.com>
2021-05-22 14:21:59 +0100

    Added project for tests to solution. Added test for Get all Candidates query.

---
+++ tests/UnitTests/Application/Candidates/Queries/TestGetAll.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Candidates.Queries.GetAll;
+using Application.Common.Interfaces.Repositories;
+using Application.Common.Mappings;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace UnitTests.Application.Candidates.Queries
+{
+    public class TestGetAll
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ICandidateRepository> _repository;
+
+        public TestGetAll()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            
+            _mapper = config.CreateMapper();
+            _repository = new Mock<ICandidateRepository>();
+        }
+        
+        [Fact]
+        public async Task ShouldReturnAllCandidates()
+        {
+            var candidates = new List<Candidate>
+            {
+                new(), new(), new()
+            };
+
+            _repository.Setup(x => x.GetAll()).ReturnsAsync(candidates);
+            
+            var query = new GetAllQueryHandler(_repository.Object, _mapper);
+
+            var response = await query.Handle(new GetAllQuery(), CancellationToken.None);
+            
+            Assert.Equal(candidates.Count, response.Candidates.Count());
+        }
+    }
+}
