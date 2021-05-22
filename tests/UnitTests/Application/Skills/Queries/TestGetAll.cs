3dffca08eba974cdef3086998b66864ae0f77934
Steven Blake <s.blake.191289@gmail.com>
2021-05-22 18:28:08 +0100

    Added test for get all skills query.

---
+++ tests/UnitTests/Application/Skills/Queries/TestGetAll.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces.Repositories;
+using Application.Common.Mappings;
+using Application.Skills.Queries.GetAll;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace UnitTests.Application.Skills.Queries
+{
+    public class TestGetAll
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ISkillRepository> _repository;
+
+        public TestGetAll()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            
+            _mapper = config.CreateMapper();
+            _repository = new Mock<ISkillRepository>();
+        }
+        
+        [Fact]
+        public async Task ShouldReturnAllSkills()
+        {
+            var skills = new List<Skill>
+            {
+                new(), new(), new()
+            };
+
+            _repository.Setup(x => x.GetAllAsync()).ReturnsAsync(skills);
+            
+            var handler = new GetAllQueryHandler(_mapper, _repository.Object);
+
+            var response = await handler.Handle(new GetAllQuery(), CancellationToken.None);
+            
+            Assert.Equal(skills.Count, response.Skills.Count());
+        }
+    }
+}
